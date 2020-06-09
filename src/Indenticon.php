acecb7a68afb3c1c4cf7b522c2667b672eef5766
ChurakovMike <churakovmike@mail.ru>
2020-06-09 20:04:50 +0700

    * add main identicon structure

--- src/Indenticon.php
+++ src/Indenticon.php
@@ -2,10 +2,43 @@
 
 namespace ChurakovMike\Identicon;
 
+use ChurakovMike\Identicon\Interfaces\GeneratorInterface;
+
 /**
  * Class Identicon
  * @package ChurakovMike\Identicon
+ *
+ * @property GeneratorInterface|null $generator
  */
 class Identicon
 {
+    /**
+     * @var GeneratorInterface|null $generator
+     */
+    private $generator;
+
+    /**
+     * Identicon constructor.
+     *
+     * @param GeneratorInterface|null $generator
+     */
+    public function __construct(GeneratorInterface $generator = null)
+    {
+        $this->generator = $generator;
+    }
+
+    /**
+     * @param GeneratorInterface $generator
+     */
+    public function setGenerator(GeneratorInterface $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function generate() {}
+
+
 }
